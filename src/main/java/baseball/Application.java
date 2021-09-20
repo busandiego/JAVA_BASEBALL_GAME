fd933cb1473179f28d1524f2d34b82c1bccfc3a6
suin lee <silee@weedscomm.com>
2021-09-20 21:43:09 +0900

    class structure

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,35 +1,13 @@
 package baseball;
 
-import baseball.domain.Calculator;
-
+/*
+  1. 기능을 가지고 있는 클래스를 인스턴스화(=객체)한다.
+  2. 필요한 기능을 각 인스턴스가 수행하게 한다. (의인화)
+  3. 각 결과를 합친다.
+*/
 public class Application {
 
-
     public static void main(String[] args) {
-
-        Calculator teacher = new Calculator();
-        System.out.println(teacher.add(1, 3)); //4
-        System.out.println(teacher.shareResult); // 4
-
-        Calculator calculator = new Calculator();
-        System.out.println( calculator.add(1, 2));
-        System.out.println("result 1: " + calculator.result);
-
-        System.out.println(calculator.add(3, 4));
-        System.out.println("result 2: " + calculator.result);
-        // result의 값이 서로 다르다 인스턴스 변수라서 공유되지 않음
-
-
-        System.out.println(calculator.add(2000, 21)); // 2021
-        System.out.println(calculator.shareResult); // 2021
-
-        System.out.println(teacher.result); // 4
-        System.out.println(teacher.shareResult); // 2021  -> 공유되는 값이다
-        System.out.println(Calculator.shareResult); // 2021  -> 공유되는 값이다
-
-        System.out.println(Calculator.SHARE_BIRTHDAY);
-
-
 
     }
 }
