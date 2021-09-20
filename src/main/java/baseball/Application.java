34c1d0434181eff42c44917ae7990da1ae1f5f4a
suin lee <silee@weedscomm.com>
2021-09-20 21:24:36 +0900

    class variable과 instance variable의 차이

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,6 +12,14 @@ public class Application {
         System.out.println(teacher.shareResult); // 4
 
         Calculator calculator = new Calculator();
+        System.out.println( calculator.add(1, 2));
+        System.out.println("result 1: " + calculator.result);
+
+        System.out.println(calculator.add(3, 4));
+        System.out.println("result 2: " + calculator.result);
+        // result의 값이 서로 다르다 인스턴스 변수라서 공유되지 않음
+
+
         System.out.println(calculator.add(2000, 21)); // 2021
         System.out.println(calculator.shareResult); // 2021
 
@@ -19,7 +27,7 @@ public class Application {
         System.out.println(teacher.shareResult); // 2021  -> 공유되는 값이다
         System.out.println(Calculator.shareResult); // 2021  -> 공유되는 값이다
 
-
+        System.out.println(Calculator.SHARE_BIRTHDAY);
 
 
 
